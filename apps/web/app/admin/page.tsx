5fb607e3f302a45f043e25b6e72b114f5ab7f147
unknown <shashankbud80@gmail.com>
2024-11-21 12:53:04 +0530

    first commit

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { Admin } from "@repo/ui/admin";
+
+export default function(){
+    return (
+        <div> hey there <Admin />
+        </div>
+    )
+}
---
+++ apps/web/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+  <div>
+<Button appName="web app">
+  hey there
+</Button>
+  </div>
+
+  )
+}
